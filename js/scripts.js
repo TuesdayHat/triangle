24a60677fae2bddab69a4680eb873f6474f78231
Adam Calhoun and julia sheremet <julia@>
2017-11-30 14:12:34 -0800

    use debugger to correct issue and place buttom inside the form tag

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
-$(document).ready(function(){
-  $("form#sides").submit(function(event){
+$(document).ready(function() {
+  $("form#sides").submit(function(event) {
     event.preventDefault();
-    debugger;
-    var sideA = parseInt($("#sideAA"));
-    var sideB = parseInt($("#sideBB"));
-    var sideC = parseInt($("#sideCC"));
+    //debugger;
 
-    if ((sideA+sideB<sideC || sideA+sideC<sideB || sideC+sideB<sideA)){
-      $("#output").text("Not A Triangle")
-    } else if (sideA === sideB && sideA === sideC && sideB === sideC) {
-      $("#output").text("Equilateral")
+    var sideA = parseInt($("input#sideAA").val());
+    //console.log("sideA: " + sideA);
+    var sideB = parseInt($("input#sideBB").val());
+    var sideC = parseInt($("input#sideCC").val());
+
+    if ((sideA + sideB <= sideC) || (sideA+sideC<sideB) || (sideC+sideB<sideA)){
+      $("#output").text("Not A Triangle");
+    } else if ((sideA === sideB) && (sideA === sideC) && (sideB === sideC)) {
+      $("#output").text("Equilateral");
     } else if (sideA===sideB || sideA===sideC || sideC===sideB ) {
-      $("#output").text("Isoscelese")
-    } else if (sideA !== sideB !== sideC) {
-      $("#output").text("Scalene")
+      $("#output").text("Isoscelese");
+    } else if ((sideA !== sideB) && (sideA !== sideC) && (sideB!== sideC)) {
+      $("#output").text("Scalene");
     } else {
       console.log("broken input");
     }
