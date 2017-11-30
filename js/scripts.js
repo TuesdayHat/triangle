9e820fc16d02747d45b4a9bf08c55f07a898b651
Adam Calhoun and julia sheremet <julia@>
2017-11-30 13:45:00 -0800

    update CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
   $("form#sides").submit(function(event){
     event.preventDefault();
+    debugger;
+    var sideA = parseInt($("#sideAA"));
+    var sideB = parseInt($("#sideBB"));
+    var sideC = parseInt($("#sideCC"));
 
-    var sideA = parseInt($("#sideA"));
-    var sideB = parseInt($("#sideB"));
-    var sideC = parseInt($("#sideC"));
-
-    if ((sideA+sideB<sideC || sideA+sideC<sideB || sideC+sideB<sideA){
+    if ((sideA+sideB<sideC || sideA+sideC<sideB || sideC+sideB<sideA)){
       $("#output").text("Not A Triangle")
-    } else if (sideA === sideB === sideC) {
+    } else if (sideA === sideB && sideA === sideC && sideB === sideC) {
       $("#output").text("Equilateral")
     } else if (sideA===sideB || sideA===sideC || sideC===sideB ) {
       $("#output").text("Isoscelese")
